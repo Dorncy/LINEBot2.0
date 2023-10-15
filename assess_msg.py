0da1b5a2d806de538affb79ad6d6851df2eaa542
Dorncy <zon528391@gmail.com>
2023-10-16 01:03:46 +0800

    ...

--- assess_msg.py
+++ assess_msg.py
@@ -3,7 +3,7 @@ from weather import get_weather_data
 from get_help import get_help
 from search_coutny import search_county
 from firebase_read import get_all_view, random_view_introduction
-from chatGPT import reply, reply_stablemsg
+from chatGPT import reply
 
 
 def assess(usertext):
@@ -25,7 +25,7 @@ def assess(usertext):
     elif re.search(need_help, usertext):
         return get_help()
     elif re.search(travel, usertext):
-        return random_view_introduction(search_county(usertext))
+        return get_all_view(search_county(usertext))
     else:
         return reply(usertext)
 
