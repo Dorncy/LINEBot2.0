4b7a55d74379577da21b7f654a5317f015e48a48
Dorncy <zon528391@gmail.com>
2023-10-16 21:00:04 +0800

    優化回復

--- chatGPT.py
+++ chatGPT.py
@@ -34,7 +34,7 @@ def reply_stablemsg(msg):
             {"role": "assistant", "content": "我是一個專門介紹台灣各大縣市旅有景點的機器人。"},
             {"role": "assistant", "content": "我將會推薦你一個你所選地區的景點資料作介紹。"},
             {"role": "assistant", "content": "這是我推薦的景點資料" + msg},
-            {"role": "user", "content": "簡短的用一句話介紹這個景點"}
+            {"role": "assistant", "content": "以簡短的用一句話推薦並介紹這個景點"}
         ]
     )
     remsg = response.choices[0].message.content
